9fce54910d7b4ee8fbb546d439711b682f62967e
gwm17 <gordonmccann215@gmail.com>
2024-12-01 09:13:33 -0500

    "Fix trouble, maybe add some more modes later"

--- lua/gwm17/plugins/trouble.lua
+++ lua/gwm17/plugins/trouble.lua
@@ -1,11 +1,13 @@
 return {
     {
         "folke/trouble.nvim",
-        config = function ()
-            require("trouble").setup({})
+        config = function()
+            local trouble = require("trouble")
+            trouble.setup({})
+
 
             vim.keymap.set("n", "<leader>tt", function()
-                require("trouble").toggle()
+                trouble.toggle("diagnostics")
             end)
             vim.keymap.set("n", "[t", function()
                 require("trouble").next({ skip_groups = true, jump = true })
