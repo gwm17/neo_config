2c6f399c7bfff92070dac7e60b7cb5f2f96c8d12
gwm17 <gordonmccann215@gmail.com>
2025-04-04 10:20:07 -0400

    Update to blink.cmp over cmp

--- lua/gwm17/plugins/lsp.lua
+++ lua/gwm17/plugins/lsp.lua
@@ -4,26 +4,14 @@ return {
 	dependencies = {
 		"williamboman/mason.nvim",
 		"williamboman/mason-lspconfig.nvim",
-		"hrsh7th/cmp-nvim-lsp",
-		"hrsh7th/cmp-buffer",
-		"hrsh7th/cmp-path",
-		"hrsh7th/cmp-cmdline",
-		"hrsh7th/nvim-cmp",
-		"L3MON4D3/LuaSnip",
-		"saadparwaiz1/cmp_luasnip",
+        "saghen/blink.cmp",
 		"j-hui/fidget.nvim",
 	},
 
 	config = function()
 		-- Grab the functionality of cmp & lsp to give to mason
-		local cmp = require("cmp")
-		local cmp_lsp = require("cmp_nvim_lsp")
-		local capabilities = vim.tbl_deep_extend(
-			"force",
-			{},
-			vim.lsp.protocol.make_client_capabilities(),
-			cmp_lsp.default_capabilities()
-		)
+		local blink = require("blink.cmp")
+		local capabilities = blink.get_lsp_capabilities() 
 
 		require("fidget").setup({})
 		require("mason").setup()
@@ -79,30 +67,6 @@ return {
 					})
 				end,
 			},
-		})
-
-		local cmp_select = { behavior = cmp.SelectBehavior.Select }
-		-- setup autocomplete
-		cmp.setup({
-			-- enable luasnip
-			snippet = {
-				expand = function(args)
-					require("luasnip").lsp_expand(args.body)
-				end,
-			},
-			-- Add some remaps
-			mapping = cmp.mapping.preset.insert({
-				["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-				["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-				["<C-y>"] = cmp.mapping.confirm({ select = true }),
-				["<C-Space>"] = cmp.mapping.complete(),
-			}),
-			sources = cmp.config.sources({
-				{ name = "nvim_lsp" },
-				{ name = "luasnip" },
-			}, {
-				{ name = "buffer" },
-			}),
 		})
 
 		-- Idk exactly what this does ...
