284779445bdd1a7e6afb92710d986588a96730b7
gwm17 <gordonmccann215@gmail.com>
2026-03-15 22:15:21 -0500

    Update treesitter

--- lua/gwm17/plugins/treesitter.lua
+++ lua/gwm17/plugins/treesitter.lua
@@ -1,27 +1,11 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
+    lazy = false,
     config = function()
-        require("nvim-treesitter.configs").setup({
-            -- Parsers to use
-            ensure_installed = {
-                "vimdoc", "python", "rust", "c", "cpp", "lua", "javascript",
-                "typescript", "json", "toml", "markdown", "bash",
-            },
-
-            -- Only for ensured
-            sync_install = false,
-
-            -- Auto install if not present on opening file
-            auto_install = true,
-
-            indent = { enable = true },
-
-            highlight = {
-                enable = true,
-
-            }
-
+        require("nvim-treesitter").install({
+            "vimdoc", "python", "rust", "c", "cpp", "lua", "javascript",
+            "typescript", "json", "toml", "markdown", "bash",
         })
     end
 }
