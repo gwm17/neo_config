3f7505e0933209e29e1e7b29d8cad21a3f7b47e3
gwm17 <gordonmccann215@gmail.com>
2026-05-07 21:41:42 -0500

    Swtich to fzf-lua to remove plenary, telescope

--- lua/gwm17/plugins/init.lua
+++ lua/gwm17/plugins/init.lua
@@ -1,6 +1,1 @@
-return {
-    {
-        "nvim-lua/plenary.nvim",
-        name = "plenary"
-    }
-}
+return {}
--- lua/gwm17/plugins/treesitter.lua
+++ lua/gwm17/plugins/treesitter.lua
@@ -1,11 +1,22 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    lazy = false,
-    config = function()
-        require("nvim-treesitter").install({
-            "vimdoc", "python", "rust", "c", "cpp", "lua", "javascript",
-            "typescript", "json", "toml", "markdown", "bash",
-        })
-    end
+	"nvim-treesitter/nvim-treesitter",
+	branch = "main",
+	build = ":TSUpdate",
+	lazy = false,
+	config = function()
+		require("nvim-treesitter").install({
+			"vimdoc",
+			"python",
+			"rust",
+			"c",
+			"cpp",
+			"lua",
+			"javascript",
+			"typescript",
+			"json",
+			"toml",
+			"markdown",
+			"bash",
+		})
+	end,
 }
